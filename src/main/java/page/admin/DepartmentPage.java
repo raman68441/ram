c807694ca7c23e39465aa5a397ef41abec9a2924
anjaneyulukr8 <anjaneyulukr8@gmail.com>
2025-02-13 10:26:16 +0530

    First code

--- src/main/java/page/admin/DepartmentPage.java
+++ src/main/java/page/admin/DepartmentPage.java
@@ -11,15 +11,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import core.BaseClass;
-import page.CommonPage;
-import page.DateSelectionPage;
 import page.GlobalFunctions;
 import page.PaginationPage;
 import utility.Constant;
+import utility.DateTimeiFunctions;
 
 /**
  * This class has department functionality
@@ -28,6 +28,8 @@ public class DepartmentPage extends GlobalFunctions {
 	Logger log = LogManager.getLogger(DepartmentPage.class);
 	private WebDriver driver;
 	PaginationPage paginationPage = new PaginationPage(BaseClass.getDriver());
+	DateTimeiFunctions dateTimeiFunctions = new DateTimeiFunctions();
+
 
 	public DepartmentPage(WebDriver driver) {
 		this.driver = driver;
@@ -43,14 +45,9 @@ public class DepartmentPage extends GlobalFunctions {
 	@FindBy(xpath = "//*[@role='tab' and text()='New Department '] ")
 	WebElement newDepartment;
 
-	@FindBy(xpath = "//*[@class='MuiDataGrid-virtualScroller css-1grl8tv']")
-	WebElement departmentListTable;
-
 	public String departmentColumnHeader = "//*[@class='MuiDataGrid-columnHeader MuiDataGrid-columnHeader--sortable' and  @aria-colindex='#']";
 
 	public String departmentData = "//*[@data-colindex='#']";
-
-	public String deptName;
 
 	@FindBy(xpath = "//*[@data-testid='DriveFileRenameOutlineOutlinedIcon']")
 	List<WebElement> editIcon;
@@ -58,14 +55,14 @@ public class DepartmentPage extends GlobalFunctions {
 	@FindBy(xpath = "//*[@data-testid='DeleteOutlineOutlinedIcon']")
 	List<WebElement> deleteIcon;
 
-	@FindBy(xpath = "//*[@class='MuiTypography-root MuiTypography-body2 MuiTypography-gutterBottom css-gix94l']")
+	@FindBy(xpath = "//*[contains(@class,'MuiTypography-root MuiTypography-body2 MuiTypography-gutterBottom css')]")
 	List<WebElement> newDepartmentFieldLabels;
 
 	@FindBy(xpath = "//*[text()='Cancel']")
 	WebElement cancelBtn;
 
-	@FindBy(xpath = "//*[text()='Save']")
-	WebElement saveBtn;
+	@FindBy(xpath = "//*[text()='Create']")
+	WebElement createBtn;
 
 	@FindBy(xpath = "//*[text()='Update']")
 	WebElement updateBtn;
@@ -79,19 +76,28 @@ public class DepartmentPage extends GlobalFunctions {
 	@FindBy(xpath = "//*[@placeholder='Name']")
 	WebElement departmentName;
 
-	@FindBy(id = "business_unit")
-	WebElement businessUnit;
+	@FindBy(xpath = "//input[@id='business_unit']")
+	WebElement site;
 
-	@FindBy(xpath = "//*[text()='Edit Department']")
+	@FindBy(xpath = "//*[@class='MuiTypography-root MuiTypography-h5 css-tv5qfu']")
 	WebElement editDepartmentHeader;
 
 	@FindBy(xpath = "//*[@class='MuiTypography-root MuiTypography-body1 css-j5a1ma']")
-	WebElement confirmDelete;
+	List <WebElement> confirmDelete;
+	
+	@FindBy(xpath = "//*[@role='row' and @aria-rowindex='1']")
+	WebElement firstRow;
+	
+	@FindBy(xpath ="//*[@class='MuiDataGrid-columnHeaderTitleContainerContent']")
+	List<WebElement> columnHeader;
+	
+	@FindBy(xpath = "//*[@role='grid']/div[1]/div[2]")
+	WebElement departmentListTable;
 
 	@FindBy(xpath = "//*[text()='Delete']")
 	WebElement deleteBtn;
 
-	@FindBy(xpath = "//*[@class='MuiAlert-message css-1xsto0d']")
+	@FindBy(xpath = "//div[contains(@class,'MuiAlert-message css')]")
 	WebElement departmentMessage;
 
 	@FindBy(xpath = "//*[text()='Department Name is required']")
@@ -100,14 +106,45 @@ public class DepartmentPage extends GlobalFunctions {
 	@FindBy(xpath = "//*[text()='Department Code is required']")
 	WebElement departmentCodeRequired;
 
-	@FindBy(xpath = "//*[text()='Please select a business unit']")
-	WebElement pleaseSelectBusineeUnit;
+	@FindBy(xpath = "//*[text()='Please select a Site']")
+	WebElement pleaseSelectsite;
 
 	@FindBy(xpath = "//*[@name='departmentDescription']")
 	WebElement deptDescription;
 
 	@FindBy(xpath = "//*[@data-colindex='1']")
 	List<WebElement> firstColumnData;
+	
+	@FindBy(xpath ="//button[text()='Reset Filter']")
+	WebElement resetFilterBtn;
+  
+	@FindBy(xpath = "//*[text()='Download']")
+	WebElement downloadBtn;
+	
+	@FindBy(xpath = "//*[@id='root']/div[1]/div[3]/div/div[2]/div[3]/div[2]/div/button[3]/span[1]")
+	WebElement downloadBtnEnable;
+	
+	@FindBy(xpath = "//*[@data-testid='VisibilityIcon']")
+	List<WebElement> viewIcon;
+	
+	@FindBy(id = "table-search")
+	WebElement tableSearchField;
+	
+	@FindBy(id = "activity_name")
+	WebElement nameField;
+	
+	@FindBy(id = "table-search-option")
+	WebElement tableSearchOption;
+	
+
+	@FindBy(id = "table-filter-option")
+	WebElement tableFilterOption;
+	
+	@FindBy(xpath = "//*[@class='MuiTypography-root MuiTypography-h5 css-tv5qfu']")
+	WebElement viewDepartmentHeader;
+	public static String activityCreatedTime, activityUpdatedTime, activityDeletedTime, activityName,
+	activityDescription, activitySignature;
+	public static String deptName, deptCode;
 
 	/**
 	 * verify the field name
@@ -116,6 +153,7 @@ public class DepartmentPage extends GlobalFunctions {
 	 */
 	public void verifyFieldName(String fieldName) {
 		String name = getXmlFilesData(fieldName);
+		String text = null;
 		switch (fieldName) {
 		case "department tab":
 			verifyText(department, name);
@@ -129,16 +167,19 @@ public class DepartmentPage extends GlobalFunctions {
 			break;
 		case "label department name":
 		case "label department code":
-		case "label business unit":
+		case "label site":
+		case "label site Tab":
 		case "label department description":
-		case "label edit business unit":
+		case "label edit site Tab":
+		case "label view site Tab":
 			getTextFromElementInListAndValidate(newDepartmentFieldLabels, name);
 			break;
+			
 		case "cancel":
 			verifyText(cancelBtn, name);
 			break;
-		case "save button":
-			verifyText(saveBtn, name);
+		case "create button":
+			verifyText(createBtn, name);
 			break;
 		case "warning message department name required":
 			verifyText(departmentNameRequired, name);
@@ -146,23 +187,38 @@ public class DepartmentPage extends GlobalFunctions {
 		case "warning message department code required":
 			verifyText(departmentCodeRequired, name);
 			break;
-		case "warning message please select business unit":
-			verifyText(pleaseSelectBusineeUnit, name);
+		case "warning message please select site":
+			verifyText(pleaseSelectsite, name);
 			break;
 		case "department create message":
 		case "department is allready present":
 		case "department updated successfully message":
 		case "department delete message":
+		case "department can not be deleted message":
 			verifyText(departmentMessage, name);
 			break;
-		case "confirm you delete":
-			verifyText(confirmDelete, name);
+		case "confirm your delete":
+			text = getTextFromElementInListInMentionedPosition(confirmDelete, 1);
+			compareTwoString(text, name);
 			break;
 		case "delete":
+			waitForElementToBeClickable(deleteBtn);
 			verifyText(deleteBtn, name);
 			break;
 		case "edit department header":
 			verifyText(editDepartmentHeader, name);
+			break;
+		case "reset filter":
+			verifyText(resetFilterBtn, name);
+			break;
+		case "download":
+			verifyText(downloadBtn, name);
+			break;
+		case "label download department":
+			verifyTextContent(downloadBtn, "textContent", name);
+			break;
+		case "view department header":
+			verifyText(viewDepartmentHeader, name);
 			break;
 		default:
 			Assert.fail("failed");
@@ -178,22 +234,22 @@ public class DepartmentPage extends GlobalFunctions {
 	 */
 	public void verifydepartmentTableColumnHeaders(String columnHeaders) {
 		// wait for element to load
-		waitTillAttributeDisplay(departmentListTable, "class", "MuiDataGrid-virtualScroller css-1grl8tv",
-				Duration.ofSeconds(60));
 		String columnHeaderValue = getXmlFilesData(columnHeaders);
 		String[] ExpectedColumnHeader = columnHeaderValue.split("\\|");
-		List<String> columnHeadersText = new ArrayList<>();
-		for (int i = 2; i <= ExpectedColumnHeader.length + 1; i++) {
-			String columnHeader = departmentColumnHeader.replace("#", Integer.toString(i));
-			WebElement element = driver.findElement(By.xpath(columnHeader));
-			columnHeadersText.add(getText(element));
+		waitTillAttributeDisplay(firstRow, "aria-rowindex", "1",
+				Duration.ofSeconds(60));
+		List<String> actualColumnHeadersText = new ArrayList<>();
+		for (int i = 0; i < ExpectedColumnHeader.length; i++) {
+			actualColumnHeadersText.add(getText(columnHeader.get(i)));
+			 scrollScrollBar(departmentListTable, 30, Constant.RIGHT);
 		}
+		scrollScrollBar(departmentListTable, 400, Constant.LEFT);
 		// compare expected and actual column headers
-		log.info("department column headers " + columnHeadersText);
-		Assert.assertTrue(compareArrayAndList(ExpectedColumnHeader, columnHeadersText));
+		log.info("department column headers " + actualColumnHeadersText);
+		Assert.assertTrue(compareArrayAndList(ExpectedColumnHeader, actualColumnHeadersText));
 		log.info("department column headers displayed");
 	}
-
+		
 	/**
 	 * verify total number of rows
 	 * 
@@ -241,20 +297,33 @@ public class DepartmentPage extends GlobalFunctions {
 		case "cancel":
 			clickElement(cancelBtn);
 			break;
-		case "save":
-			clickElement(saveBtn);
+		case "create":
+			clickElement(createBtn);
 			break;
 		case "update":
 			clickElement(updateBtn);
 			break;
 		case "delete":
+			waitForElementToBeClickable(deleteBtn);
 			clickElement(deleteBtn);
 			break;
 		case "edit":
 			clickOnPerticularListElement(editIcon,0);
 			break;
 		case "delete icon":
+			//waitForListElementsToBeVisible(deleteIcon);
+			waitForListElementsToBeVisible(deleteIcon);
+			//waitForElementToBeClickable(deleteBtn);
 			clickOnPerticularListElement(deleteIcon, 0);
+			break;
+		case "reset filter":
+			clickElement(resetFilterBtn);
+			break;
+		case "download":
+			clickElement(downloadBtn);
+			break;
+		case "view":
+			clickOnPerticularListElement(viewIcon, 0);
 			break;
 		default:
 			Assert.fail("failed");
@@ -278,18 +347,45 @@ public class DepartmentPage extends GlobalFunctions {
 			departmentName.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
 			enterValue(departmentName, deptName);
 			break;
+		case "department description":
+			if (deptName==null) {
+				deptName = String.format("%s%s", fieldValue, generateRandomString(4));
+			}
+			departmentName.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
+			enterValue(departmentName, deptName);
+			break;
 		case "edit department name":
 			deptName = String.format("%s%s", fieldValue, generateRandomString(4));
 			departmentName.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
 			enterValue(departmentName, deptName);
 			break;
 		case "department code":
+			if (deptCode==null) {
+				deptCode = String.format("%s%s", fieldValue, generateRandomString(2));
+			}
 			departmentCode.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
-			enterValue(departmentCode, fieldValue);
+			enterValue(departmentCode, deptCode);
 			break;
 		case "description":
 			deptDescription.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
 			enterValue(deptDescription, fieldValue);
+			break;
+		case "search":
+			tableSearchField.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+			if (text.equals("text department description")) {
+				enterValue(tableSearchField, fieldValue);
+			} else if(text.equals("text department name")) {
+			
+				enterValue(tableSearchField, fieldValue);
+			}
+			else {
+				enterValue(tableSearchField, deptName);
+			}
+			break;
+		case "edit name":
+			deptName = String.format("%s%s", fieldValue, generateRandomString(7));
+			nameField.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+			enterValue(nameField, deptName);
 			break;
 		default:
 			System.out.println("invalid name");
@@ -306,20 +402,36 @@ public class DepartmentPage extends GlobalFunctions {
 	public void validateMentionedRowNumberDataFordepartment(String data, int rowNumber) {
 		String rowData = getXmlFilesData(data);
 		String[] departmentExpectedData = rowData.split("\\|");
+		if (departmentExpectedData[0].equals("##")) {
+			departmentExpectedData[0] = deptName;
+		}
 		if (departmentExpectedData[1].equals("##")) {
-			departmentExpectedData[1] = deptName;
+			departmentExpectedData[1] = deptCode;
 		}
 		
+		if (departmentExpectedData[4].equals("##")) {
+			departmentExpectedData[4] = dateTimeiFunctions.getCurrentDateMonthNameYear("-");
+
+		}
+		if (departmentExpectedData[6].equals("##")) {
+			departmentExpectedData[6] = dateTimeiFunctions.getCurrentDateMonthNameYear("-");
+
+		}
 		int columnLength = departmentExpectedData.length;
+		rowNumber = rowNumber-1;
 		List<String> actualData = new ArrayList<>();
 		for (int i = 1; i <= columnLength; i++) {
-			String rowItem = departmentData.replace("#", Integer.toString(i));
+			String rowItem = "//*[@data-rowindex='" + Integer.toString(rowNumber) + "']/*[@data-colindex='"
+					+ Integer.toString(i) + "']";
 			WebElement element = driver.findElement(By.xpath(rowItem));
 			actualData.add(getText(element));
-			// scrollScrollBar(auditTrailTable, 90, Constant.RIGHT);
+			 scrollScrollBar(departmentListTable, 25, Constant.RIGHT);
 		}
+		 scrollScrollBar(departmentListTable, 300, Constant.LEFT);
+
 		log.info(String.format("mentioned row %s %s", rowNumber, departmentExpectedData));
 		Assert.assertTrue(compareArrayAndList(departmentExpectedData, actualData));
+
 	}
 
 	/**
@@ -331,18 +443,128 @@ public class DepartmentPage extends GlobalFunctions {
 		String value = getXmlFilesData(data);
 		WebElement element = null;
 		switch (fieldName) {
-		case "business unit":
-			clickElement(businessUnit);
-//			WebElement l = driver.findElement(By.tagName("body"));
-//			String p = l.getText();
-			element = driver.findElement(By.xpath("//*[text()='" + value + "']"));
-			clickElement(element);
+		case "site":
+			clickElement(site);////span[normalize-space()='"+value +"']
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			clickArrowAndEnter();
+			//waitForElementToBeClickable(driver.findElement(By.xpath("//*[text()='"+value +"']")));
+			//element = driver.findElement(By.xpath("//*[text()='"+value +"']"));
+			
+			//clickElement(element);
 			break;
+		case "search option":
+			clickElement(tableSearchOption);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			element = driver.findElement(By.xpath("//*[@role='option']/..//*[text()='Department Name']"));
+			clickElement(element);
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case "table filter":
+			clickElement(tableFilterOption);
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			element = driver.findElement(By.xpath("//*[@data-value='" + value + "']"));
+			clickElement(element);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		default:
 			log.error("invalid field" + fieldName);
 			Assert.fail("failed");
 			break;
 		}
 	}
+	
+	public void verifyMentionedCellDataInTable(String data, int rowNumber, int columnNumber) {
+		String rowData = null;
+		if(data.equals("text department name")) {
+			rowData = deptName;
+		} else {
+		  rowData = getXmlFilesData(data);
+		}
+		if(data.equals("text description")) {
+			rowData = deptName;
+		} else {
+		  rowData = getXmlFilesData(data);
+		}
+		
+		String rowItem = "//*[@aria-rowindex='" + Integer.toString(rowNumber) + "']/..//*[@aria-colindex='"
+				+ Integer.toString(columnNumber) + "']";
+		WebElement element = driver.findElement(By.xpath(rowItem));
+		compareTwoString(rowData, getText(element));
+	}
 
+	/**
+	 * Select from drop down
+	 * 
+	 * @param value - value or index or text
+	 */
+	public void selectDropDownfromDepartmentPage1(String data, String fieldName) {
+		String value = getXmlFilesData(data);
+		WebElement element = null;
+		String optionValue = null;
+		switch (fieldName) {
+		case "search option":
+			clickElement(tableSearchOption);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			element = driver.findElement(By.xpath("//*[@role='option']/..//*[text()='Department Name']"));
+			clickElement(element);
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case "table filter":
+			clickElement(tableFilterOption);
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			element = driver.findElement(By.xpath("//*[@data-value='" + value + "']"));
+			clickElement(element);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		default:
+			log.error("invalid field" + fieldName);
+			Assert.fail("failed");
+			break;
+		}
+	}
+	
 }
