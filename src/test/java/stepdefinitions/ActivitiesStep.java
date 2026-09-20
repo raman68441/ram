ca21d2f4a5dab829a9c2230d0e6930efda5f8e18
raman68441 <raman68441@gmail.com>
2026-09-20 18:13:29 +0530

    Refactor ActivitiesStep class for clarity and structure

--- src/test/java/stepdefinitions/ActivitiesStep.java
+++ src/test/java/stepdefinitions/ActivitiesStep.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import page.admin.SitePage;
 import page.config.ActivitiesPage;
-
+//hdjshd
 public class ActivitiesStep {
 	
 	ActivitiesPage activitiesPage = new ActivitiesPage(BaseClass.getDriver());
